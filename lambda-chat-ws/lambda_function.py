f36b81591240bb279314acd00cd26a0982bac132
Kyoungsu Park <kyopark2014@gmail.com>
2024-05-23 09:33:02 +0900

    test create_tool_calling_agent

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -324,13 +324,6 @@ def get_product_list(keyword: str) -> list:
     else:
         return []
 
-@tool
-def who_are_you():
-    """Returns the name of assistant"""
-        
-    return "경수"
-
-
 def current_time() -> str:
     """
     Get current time and return it.
@@ -395,7 +388,7 @@ def get_weather_info(city: str) -> str:
 
 
 def run_tool_calling_agent(connectionId, requestId, chat, query):
-    toolList = "who_are_you, get_current_time, get_product_list, get_weather_info"
+    toolList = "get_current_time, get_product_list, get_weather_info"
     # system = f"You are a helpful assistant. Make sure to use the {toolList} tools for information."
     system = f"다음의 Human과 Assistant의 친근한 이전 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant의 이름은 서연이고, 모르는 질문을 받으면 솔직히 모른다고 말합니다. 답변에 필요한 정보는 다움의 tools를 이용해 수집하세요. Tools: {toolList}"
             

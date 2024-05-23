d22c7262bc7d91a284efac1cbebfd8d859686ec7
Kyoungsu Park <kyopark2014@gmail.com>
2024-05-23 18:53:18 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -412,8 +412,11 @@ def get_weather_info(city: str) -> str:
 
 def run_tool_calling_agent(connectionId, requestId, chat, query):
     toolList = "get_system_time, get_product_list, get_weather_info"
-    #system = f"You are a helpful assistant. Make sure to use the {toolList} tools for information."
-    system = f"You are a helpful assistant. Make sure to use the get_system_time tool for information."
+    tool_name = ", ".join((t.name for t in tools))
+    print('tool_name: ', tool_name)
+    
+    system = f"You are a helpful assistant. Make sure to use the {toolList} tools for information."
+    # system = f"You are a helpful assistant. Make sure to use the get_system_time tool for information."
     #system = f"다음의 Human과 Assistant의 친근한 이전 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant의 이름은 서연이고, 모르는 질문을 받으면 솔직히 모른다고 말합니다. 답변에 필요한 정보는 다움의 tools를 이용해 수집하세요. Tools: {toolList}"
             
     prompt = ChatPromptTemplate.from_messages(
@@ -507,7 +510,7 @@ Final Answer: the final answer to the original input question
 
 Begin!
 
-Always answer in Korean.
+Answer the Final Answer in Korean.
 
 Question: {input}
 Thought:{agent_scratchpad}

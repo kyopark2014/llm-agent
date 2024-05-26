d3fe586f5b9d35bb98ccb73da52ef793a8121dc1
Kyoungsu Park <kyopark2014@gmail.com>
2024-05-26 22:17:15 +0900

    update api

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -369,11 +369,7 @@ def get_book_list(keyword: str) -> str:
     
 @tool
 def get_current_time (format: str = f"%Y-%m-%d %H:%M:%S")->str:
-    """
-    retrive system time to earn current date and time
-    format: the format of date and time to retrieve
-    return: current date and time by the given format
-    """
+    """Returns the current date and time in the specified format"""
     
     format = format.replace('\'','')
     

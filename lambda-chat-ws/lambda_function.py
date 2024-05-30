93dd5e31d6620e68aba3a160eaa7c583ce18fc3d
Kyoungsu Park <kyopark2014@gmail.com>
2024-05-30 13:58:17 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -496,7 +496,7 @@ def get_system_time() -> list:
     return result
 
 @tool
-def get_weather_info(city: str = "Seoul") -> str:
+def get_weather_info(city: str = "Seoul" or 'Seoul') -> str:
     """
     retrieve weather information by city name and then return weather statement.
     city: the name of city to retrieve

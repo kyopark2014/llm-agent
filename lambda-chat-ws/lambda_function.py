e781a2f37d753e02bc9c957a80677f3b657ec83e
Kyoungsu Park <kyopark2014@gmail.com>
2024-06-08 13:46:03 +0900

    langgraph agent

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -991,6 +991,8 @@ def run_langgraph_agent(connectionId, requestId, chat, query):
             
             #if 'agent_outcome' in value:
             #    print('agent_outcome: ', value['agent_outcome'])
+        if 'agent_outcome' in result:
+            print('--> agent_outcome: ', result['agent_outcome'])
         
     msg = ""
     if 'agent_outcome' in result:
